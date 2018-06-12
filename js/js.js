8963bca063228151ffc1a2b3be16104155a4f0d3
Eitán Abelson <llovia@gmail.com>
2018-06-12 00:46:55 -0300

    Se logra la funcionalidad pero con una funcion que realiza un loop etetno.

--- js/js.js
+++ js/js.js
@@ -1,3 +1,5 @@
+let bolillasElegidas = [91];
+
 //Creo la grilla de bolillas
   for (let x = 1; x <= 90; x++) {
     let bolilla = document.createElement('div');
@@ -6,10 +8,34 @@
     bolilla.innerText = x; 
     document.getElementById('container').appendChild(bolilla);
   }
+
 //al clickear en tómbola se elige una ficha al azar 
   $('.tombola').click(
-    () => {
+    function elegirBolilla () {
       let bolElegida = Math.floor(Math.random() * 90);
-      console.log(bolElegida);
+      console.log('random' + bolElegida);
+      bolillasElegidas.forEach(element => {
+        if (element !== bolElegida) {
+          let bolUnica = bolElegida;
+          bolillasElegidas.push(bolUnica);
+          $('#' + bolUnica).addClass('elegida');
+        }else{
+          elegirBolilla();
+        }
+      });
     }
+    // function elegirBolilla () {
+    //   let bolElegida = Math.floor(Math.random() * 90);
+    //   console.log('random' + bolElegida);
+    //   bolillasElegidas.forEach(element => {
+    //     if (element === bolElegida) {
+    //      elegirBolilla();
+    //     }else{
+    //       let bolUnica = bolElegida;
+    //       bolillasElegidas.push(bolUnica);
+    //       $('#' + bolUnica).addClass('elegida');
+    //     }
+    //   });
+    // }
   );
+
