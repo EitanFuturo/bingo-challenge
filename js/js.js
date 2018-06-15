ecf60c50e91852ccaed5f0774397aabe36d7bdb6
Eitán Abelson <llovia@gmail.com>
2018-06-15 00:30:32 -0300

    Se terminan las funcionalidades principales.

--- js/js.js
+++ js/js.js
@@ -1,15 +1,38 @@
+let bolillas = [];
+
 //Creo la grilla de bolillas
-  for (let x = 1; x <= 90; x++) {
-    let bolilla = document.createElement('div');
-    bolilla.className = 'bolilla';
-    bolilla.id = x;    
-    bolilla.innerText = x; 
-    document.getElementById('container').appendChild(bolilla);
-  }
+for (let x = 1; x <= 90; x++) {
+  let bolilla = document.createElement('div');
+  bolilla.className = 'bolilla';
+  bolilla.id = x;    
+  bolilla.innerText = x; 
+  document.getElementById('container').appendChild(bolilla);
+  bolillas.push(x);
+
+}
 //al clickear en tómbola se elige una ficha al azar 
   $('.tombola').click(
-    () => {
-      let bolElegida = Math.floor(Math.random() * 90);
-      console.log(bolElegida);
+    function elegir () {
+      let bolElegida = Math.floor(Math.random() * bolillas.length);
+      $('#container').children().removeClass('ultima')      
+      $('#' + bolillas[bolElegida]).addClass('elegida');
+      $('#' + bolillas[bolElegida]).addClass('ultima');   
+      bolillas.splice(bolElegida, 1);
     }
   );
+
+document.addEventListener('keydown', (e) => {
+  if(e.keyCode == 32 && e.target == document.body) {
+    e.preventDefault();
+  }
+  reiniciar();
+});
+
+function reiniciar () {
+  $('#container').children().removeClass('elegida');        
+  $('#container').children().removeClass('ultima');
+  bolillas = [];          
+  for (let x = 1; x <= 90; x++) {
+    bolillas.push(x);
+  }
+}
